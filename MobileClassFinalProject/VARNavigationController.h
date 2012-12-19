f0a13c0e4a93d2b2bd39101ec64ca2049b558b28
Admin <admin@Adminteki-MacBook.local>
2012-12-19 21:56:55 +0800

    disable auto rotate

---
+++ MobileClassFinalProject/VARNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  VARNavigationController.h
+//  MobileClassFinalProject
+//
+//  Created by Admin on 12/12/19.
+//  Copyright (c) 2012年 VAR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VARNavigationController : UINavigationController
+
+@end
